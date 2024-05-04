99a015f08b0ecc9258f115084e46e330ea0b071c
DELL <mattahuasi@gmail.com>
2024-05-03 23:37:36 -0400

    Migraciones creadas

---
+++ database/migrations/2024_05_03_230145_create_retail_outlets_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('retail_outlets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('address');
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('retail_outlets');
+    }
+};
---
+++ database/migrations/2024_05_03_232109_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('detail');
+            $table->date('order_date');
+            $table->date('delivery_deadline');
+            $table->foreignId('customer_id')->constrained('customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
