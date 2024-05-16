ff5b4586011e582af37fcbb0cda83235bf249f24
mattahuasi <mattahuasi@gmail.com>
2024-05-16 18:50:19 -0400

    card stats agregado

---
+++ resources/js/Components/cards/CardStat.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+const props = defineProps({
+    title: {
+        type: String,
+        default: "Title",
+    },
+    quantity: {
+        type: Number,
+        default: 8282,
+    },
+    iconBg: {
+        type: String,
+        default: "bg-indigo-600",
+    },
+    iconName: {
+        type: String,
+        default: "hi-solid-exclamation-circle",
+    },
+    route: {
+        type: String,
+        default: route("dashboard"),
+    },
+});
+</script>
+<template>
+    <Link :href="route">
+        <div
+            class="flex items-center px-5 mb-4 break-words xl:mb-0 py-6 bg-white rounded-md shadow-sm shadow-gray-300"
+        >
+            <div
+                class="text-white bg-opacity-75 p-3 text-center inline-flex items-center justify-center w-12 h-12 rounded-full"
+                :class="iconBg"
+            >
+                <v-icon :name="iconName" />
+            </div>
+            <div class="mx-5">
+                <h4 class="text-2xl font-semibold text-gray-700">
+                    {{ quantity }}
+                </h4>
+                <div class="text-gray-500 text-sm">{{ title }}</div>
+            </div>
+        </div>
+    </Link>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import ButtonAdd from "@/Components/buttons/ButtonAdd.vue";
 import CardData from "@/Components/cards/CardData.vue";
-import CardStats from "@/Components/CardStats.vue";
+import CardStat from "@/Components/cards/CardStat.vue";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
 import {
@@ -29,6 +29,10 @@ ChartJS.register(
     PointElement,
     LineElement
 );
+
+const employees = 500;
+const entries = 700;
+const exits = 258;
 const lineData = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
@@ -47,7 +51,6 @@ const barData = {
         "Materia Prima 3",
         "Materia Prima 4",
         "Materia Prima 5",
-
     ],
     datasets: [
         {
@@ -82,9 +85,9 @@ const options = {
     responsive: true,
 };
 
-import { GoogleMap, Marker } from 'vue3-google-map'
+import { GoogleMap, Marker } from "vue3-google-map";
 
-const center = { lat: 40.689247, lng: -74.044502 }
+const center = { lat: 40.689247, lng: -74.044502 };
 </script>
 
 <template>
@@ -93,46 +96,47 @@ const center = { lat: 40.689247, lng: -74.044502 }
     <authenticated-layout>
         <card-data title="Panel de control">
             <div class="flex flex-wrap -mx-6">
-      <div class="w-full px-6 lg:w-1/3">
-        <card-stat
-          title="Empleados registrados"
-          quantity="employees"
-          icon-bg="'bg-indigo-600'"
-          icon-name="'hi-solid-user-group'"
-        />
-      </div>
-      <div class="w-full px-6 lg:w-1/3">
-        <card-stat
-          title="Entradas registradas hoy"
-          :quantity="entries"
-          :icon-bg="'bg-blue-600'"
-          :icon-name="'hi-solid-login'"
-        />
-      </div>
-      <div class="w-full px-6 lg:w-1/3">
-        <card-stat
-          title="Salidas registradas hoy"
-          :quantity="exits"
-          :icon-bg="'bg-pink-600'"
-          :icon-name="'hi-solid-logout'"
-        />
-      </div>
-    </div>
-            <h2>Gestionar Puntos de Venta</h2>
-            <div
-                    class="flex flex-col justify-between md:flex-row gap-2 w-full"
-                >
-                    <button-add :href="route('retail-outlets.create')"
-                        >Crear Ruta</button-add
-                    >
+                <div class="w-full px-6 lg:w-1/3">
+                    <card-stat
+                        title="Empleados registrados"
+                        :quantity="employees"
+                        :icon-bg="'bg-indigo-600'"
+                        :icon-name="'hi-solid-user-group'"
+                        :route="route('users.index')"
+                    />
                 </div>
+                <div class="w-full px-6 lg:w-1/3">
+                    <card-stat
+                        title="Entradas registradas hoy"
+                        :quantity="entries"
+                        :icon-bg="'bg-blue-600'"
+                        :icon-name="'hi-solid-login'"
+                        :route="route('entries.index')"
+                    />
+                </div>
+                <div class="w-full px-6 lg:w-1/3">
+                    <card-stat
+                        title="Salidas registradas hoy"
+                        :quantity="exits"
+                        :icon-bg="'bg-pink-600'"
+                        :icon-name="'hi-solid-logout'"
+                        :route="route('outflows.index')"
+                    />
+                </div>
+            </div>
+            <h2>Gestionar Puntos de Venta</h2>
+            <div class="flex flex-col justify-between md:flex-row gap-2 w-full">
+                <button-add :href="route('retail-outlets.create')"
+                    >Crear Ruta</button-add
+                >
+            </div>
             <GoogleMap
-                    :api-key="AIzaSyC6zamak9gpkkMDnqoJKYoC7qhH4e4Sx6c"
-                    style="width: 100%; height: 400px"
-                    :center="center"
-                    :zoom="15"
-                    >
-                    <Marker :options="{ position: center }" />
+                :api-key="AIzaSyC6zamak9gpkkMDnqoJKYoC7qhH4e4Sx6c"
+                style="width: 100%; height: 400px"
+                :center="center"
+                :zoom="15"
+            >
+                <Marker :options="{ position: center }" />
             </GoogleMap>
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 <div class="w-full">
@@ -150,5 +154,4 @@ const center = { lat: 40.689247, lng: -74.044502 }
             </div>
         </card-data>
     </authenticated-layout>
-
 </template>
